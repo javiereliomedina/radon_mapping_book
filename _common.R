a0eaf4b58041ddc6b3aff9d9f0f028a856a860af
Javier Elio <javiereliomedina@gmail.com>
2021-04-12 07:21:27 +0200

    update

---
+++ _common.R
@@ -0,0 +1,30 @@
+set.seed(1014)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#",
+  collapse = knitr::is_latex_output(),
+  cache = TRUE,
+  #  out.width = "70%",
+  warning = FALSE,
+  message = FALSE,
+  include = TRUE,
+  echo = TRUE,
+  fig.align = 'center',
+  fig.pos = "H"
+  #  fig.width = 6,
+  #  fig.asp = 0.618,  # 1 / phi
+  #  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+options(stars.crs = 17)
+options(knitr.kable.NA = "-")
+mapview::mapviewOptions(fgb = FALSE)
+
+# for units: (?)
+Sys.setenv(UDUNITS2_XML_PATH="")
+if (knitr::is_latex_output())
+  options(width = 66)
+  #options(width = 72)
+
